7d7977482b9869cb71b18dc8c48f6a1e9e9f9d68
Cecilia Acciarini <ceci.acciarini@gmail.com>
2018-11-05 16:03:37 -0300

    JavaScript (Sin terminar)

---
+++ Javasript/javascript-ej1-6.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+    $('section.hidden').fadeIn(1000).removeClass('hidden');
+   }    
+)
+
+function myFunction(){
+                var req = new XMLHttpRequest();
+                req.open('Get', "http://api.icndb.com/jokes/random", true);
+                req.onreadystatechange =  function () {
+                    if (req.readyState == 4) {
+                       if(req.status == 200){
+                            console.log("Request done successfully");
+                            document.getElementById('rta').innerHTML = req.responseText;
+                        }
+                    else
+                        {console.log("Error loading page");
+                        document.getElementById('rta').innerHTML = ("Error loading page\n");
+                        }
+                    }
+                    
+                  }
+                req.send(null); 
+            }
+
+
+
+
+          
