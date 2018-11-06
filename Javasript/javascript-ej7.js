8aa5a3b42c2da1b1d5dc67b49be2a5361c4e9484
Cecilia Acciarini <ceci.acciarini@gmail.com>
2018-11-06 15:17:38 -0300

    Modifications

--- Javasript/javascript-ej7.js
+++ Javasript/javascript-ej7.js
@@ -1,37 +1,41 @@
 
 $(document).ready(function() {
     $('section.hidden').fadeIn(1000).removeClass('hidden');
-   });
-
-document.getElementById('btn').addEventListener("click", function(){
-    var showData = function(data){
-        document.getElementById('rta').innerHTML = data.value.joke;
-    }
-    var config = new Object();
-    config.url = "http://api.icndb.com/jokes/random";
-    config.callback = showData;
-    getFetch(config);
+   
+    document.getElementById('btn').addEventListener("click", function(){
+        var config = new Object();
+        config.url = "http://api.icndb.com/jokes/random";
+        config.callback = showData;
+        getFetch(config);
+    });
 });
 
-function makeACall(config) {
-    return new Promise (function(resolve,reject){
-                        var req = new XMLHttpRequest();
-                        req.open('Get', config.url, true);
-                        req.addEventListener("load",function() {
-                                        if (req.readyState == 4 && req.status == 200){
-                                            console.log("Request done successfully");
-                                            var resp = resolve(req.responseText);
-                                            config.callback(JSON.parse(resp)); 
+function showData(data){
+    document.getElementById('rta').innerHTML = data.value.joke;
+}
 
-                                        }else {
-                                            reject("Error code: " + req.statusText);
-                                            document.getElementById('rta').innerHTML="Server error: " + req.statusText; 
-                                            document.getElementById('rta').style.color = "red";
-                                        }
+function makeACall(config) 
+{
+    return new Promise (function(resolve,reject)
+    {
+        var req = new XMLHttpRequest();
+        req.open('Get', config.url, true);
+        req.addEventListener("load",function() {
+            if (req.readyState == 4 && req.status == 200)
+            {
+                console.log("Request done successfully");
+                config.callback(JSON.parse(resolve(req.responseText))); 
+            }
+            else
+            {
+                reject("Error code: " + req.statusText);
+                document.getElementById('rta').innerHTML="Server error: " + req.statusText; 
+                document.getElementById('rta').style.color = "red";
+            }
                                     
-                            })
-                            req.send(null)
-                        })        
+        })
+        req.send(null)
+    })        
 }
 
 
