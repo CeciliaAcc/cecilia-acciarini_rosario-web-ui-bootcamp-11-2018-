1094968357f2e8a44481dcab81dcfbfe7f1b8b2b
Cecilia Acciarini <ceci.acciarini@gmail.com>
2018-11-14 16:33:00 -0300

    javascript oop

---
+++ Javascript OOP/evenEmitter.js
@@ -0,0 +1,29 @@
+
+export default class EventEmitter{
+
+    constructor(){
+        this.events = {}
+    }
+
+    on(eventName, callback){
+        if (this.events[eventName]){
+            this.events[eventName].push(callback)
+        }else{
+            this.events[eventName] = [callback]
+        }
+    }
+
+    emit(eventName){
+        if(this.events[eventName]){
+            this.events[eventName].forEach(callback => {
+                callback(eventName)
+            })
+        }
+    }
+
+    off(eventName){
+            if (this.events[eventName]){
+                delete this.events[eventName]    
+            }
+    }
+}
---
+++ Javascript OOP/logger.js
@@ -0,0 +1,11 @@
+
+export class Logger{
+
+    constructor(){
+
+    }
+
+    log(info){
+        console.log('The ' + info + ' event has been emitted');
+    }
+}
